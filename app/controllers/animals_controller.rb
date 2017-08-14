b5d26fe42b88b2adc494967dff0f3ee9beba2a5f
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-08-14 17:32:12 +0200

    Adding CRUD on AnimalsController

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,3 +1,48 @@
 class AnimalsController < ApplicationController
+  before_action :set_animal, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.user = current_user
+    if @animal.save
+      redirect_to animal_path(@animal)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @animal.update(animal_params)
+    redirect_to animal_path(@animal)
+  end
+
+  def destroy
+    @animal.destroy
+    redirect_to animals_path
+  end
+
+  private
+
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
+
+  def animal_params
+    params.require(:animal).permit(:title, :address, :description, :daily_price)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814134939) do
+ActiveRecord::Schema.define(version: 20170814140007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20170814134939) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_animals_on_user_id", using: :btree
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "user_id"
+    t.integer  "animal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["animal_id"], name: "index_bookings_on_animal_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +55,6 @@ ActiveRecord::Schema.define(version: 20170814134939) do
   end
 
   add_foreign_key "animals", "users"
+  add_foreign_key "bookings", "animals"
+  add_foreign_key "bookings", "users"
 end
