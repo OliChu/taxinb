8348d5b741162a9eecfda029ea634559c02277a1
Morgan Autret <morgan.autret@gmail.com>
2017-08-14 18:25:58 +0200

    adding skipping login

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,5 @@
 class AnimalsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   before_action :set_animal, only: [:show, :edit, :update, :destroy]
 
   def index
