0c04134a1813e13457e94bdb331e7ce101b3e885
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-08-15 13:56:10 +0200

    Adding Price and Status in Bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,6 +19,8 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.animal = @animal
     @booking.user = current_user
+    @booking.price = (@booking.end_date - @booking.end_date) * @animal.daily_price
+    @booking.status = "Pending"
     if @booking.save
       redirect_to animal_path(@animal)
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815085033) do
+ActiveRecord::Schema.define(version: 20170815115300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170815085033) do
     t.integer  "animal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "price"
+    t.string   "status"
     t.index ["animal_id"], name: "index_bookings_on_animal_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
