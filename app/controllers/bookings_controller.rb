78e7e1685a01716d294df2fd6865f32cddd337b7
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-08-15 13:58:01 +0200

    Adding Price and Status to Edit Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -36,6 +36,8 @@ class BookingsController < ApplicationController
   def update
     @booking = Booking.find(params[:id])
     @booking.update(booking_params)
+    @booking.price = (@booking.end_date - @booking.end_date) * @animal.daily_price
+    @booking.status = "Pending"
     @animal = @booking.animal
     redirect_to animal_path(@animal)
   end
