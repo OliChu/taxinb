7d6cc29f7857ca9fa96c6e16146a0c7bdd170adf
Vincent Salamand <salamandvincent@gmail.com>
2017-08-15 14:02:04 +0200

    add controller & view for user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: :show
+
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
 
   root to: 'animals#index'
 
